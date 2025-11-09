a96da01f20e8b2c85c16f4555ccfb331ad3f89a8
alkurbi2080-lang <alkurbi2080@gmail.com>
2025-11-09 17:49:51 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', e => { /* optional caching later */ });
+
+// (Optional) Add caching later if you want offline. For now, keep it simple:
+self.addEventListener('fetch', () => {});
